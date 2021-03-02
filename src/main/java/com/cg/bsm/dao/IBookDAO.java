c8eb617f26d2af3195e92befd5d000abe22972af
Rudrarajusaikrishna33 <rudrarajusaikrishna2130@gmail.com>
2021-03-02 13:52:35 +0530

    BookStore

---
+++ src/main/java/com/cg/bsm/dao/IBookDAO.java
@@ -0,0 +1,14 @@
+package com.cg.bsm.dao;
+
+import com.cg.bsm.model.Book;
+import com.cg.bsm.exception.BookStoreException;
+import java.util.List;
+
+public interface IBookDAO {	
+	String add(Book book) throws BookStoreException;
+	boolean delete(String bcode)throws BookStoreException;
+	Book get(String bcode) throws BookStoreException;;
+	List<Book> getAll() throws BookStoreException;;
+	boolean update(Book book) throws BookStoreException;
+	void persist()throws BookStoreException;
+}
